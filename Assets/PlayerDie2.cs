867fe0013557d3f4b56801ef7d2d623b63a2e5b3
RainixGD <gmdrainix@gmail.com>
2024-07-14 22:28:31 +0300

    RainixGD

--- Assets/PlayerDie2.cs
+++ Assets/PlayerDie2.cs
@@ -4,6 +4,8 @@ public class PlayerDie2 : MonoBehaviour
 {
     [SerializeField] public GameObject gameOverPanel;
     [SerializeField] public AudioSource dieSound;
+    [SerializeField] public AudioSource fireInTheHoleSound;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -13,6 +15,15 @@ public class PlayerDie2 : MonoBehaviour
             dieSound.Play();
 
             Time.timeScale = 0.0f;
+        }
+        if (collision.CompareTag("fire_in_the_hole"))
+        {
+            fireInTheHoleSound.Play();
+        }
+        if (collision.CompareTag("pad"))
+        {
+            var rb = Player.instance.GetComponent<Rigidbody2D>();
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + 20);
         }
     }
 }
